d0416446dd3f2e58d9c4077a749b56332193ded3
Emmanuel Selase Tormeti <emmasela04@gmail.com>
2024-04-29 10:47:17 +0000

    fixed bugs

--- controllers/collections.js
+++ controllers/collections.js
@@ -5,26 +5,33 @@ const Book = require('../models/book')
 
 
 collectionsRouter.get('/', async (request, response) => {
-  const { sort } = request.query
-  let sortStage = {}
+  const { sort } = request.query;
+  let sortStage = {};
 
   if (sort) {
     const sortOptions = sort.split(',').map(option => {
       const [field, order] = option.split(':');
       return { [field]: order === 'desc' ? -1 : 1 };
-    })
+    });
+
+    sortStage = { $sort: Object.assign({}, ...sortOptions) };
+  }
   
-    sortStage = { $sort: Object.assign({}, ...sortOptions) }
+  let aggregationPipeline = [
+    { $group: { _id: '$genre', bookCount: { $sum: 1 } } },
+    { $project: { genre: '$_id', bookCount: 1, _id: 0 } }
+  ];
+
+  if (Object.keys(sortStage).length !== 0) {
+    aggregationPipeline.push(sortStage);
   }
 
+  const collection = await Book.aggregate(aggregationPipeline);
 
-  const collection = await Book.aggregate([
-    { $group: { _id: '$genre', bookCount: { $sum: 1 } } },
-    { $project: { genre: '$_id', bookCount: 1, _id: 0 } },
-    sortStage
-  ])
-  response.json(collection)
-})
+  response.json(collection);
+});
+
+
 
 
 module.exports = collectionsRouter
